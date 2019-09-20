6983f114c2c36f5b26fde3b9659f8ccaf745b65f
Alex Farmer <farmale10@gmail.com>
2019-09-20 17:23:07 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,18 @@
 
 def Post
 
+  @@all = []
+  
+  attr_accessor :title, :artist
 
+  def initialize(title)
+    @title = title
+    @@all.push(self)
+  end
 
-
+ def self.all 
+   @@all
+ end 
 
 
 
